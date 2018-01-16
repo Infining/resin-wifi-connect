230e97ad3031cebb20aa3984ddb4f92702bba0c5
majorz <zahari@resin.io>
2018-01-16 21:01:05 +0200

    Start script and Dockerfile updates

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,23 +4,23 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
 # Choose a condition for running WiFi Connect according to your use case:
 
-# 1. Is there Internet connectivity?
-# nmcli -t g | grep full
-
-# 2. Is there a default gateway?
+# 1. Is there a default gateway?
 # ip route | grep default
 
-# 3. Is there an active connection?
-# nmcli -t c show --active | grep :
+# 2. Is there Internet connectivity?
+# nmcli -t g | grep full
+
+# 3. Is there Internet connectivity via a google ping?
+# wget --spider http://google.com 2>&1
 
 # 4. Is there an active WiFi connection?
-nmcli -t c show --active | grep 802-11-wireless
+iwgetid -r
 
 if [ $? -eq 0 ]; then
-    printf 'Skipping WiFi Connect'
+    printf 'Skipping WiFi Connect\n'
 else
-    printf 'Starting WiFi Connect'
-    ./resin-wifi-connect
+    printf 'Starting WiFi Connect\n'
+    ./wifi-connect
 fi
 
 # Start your application here.
